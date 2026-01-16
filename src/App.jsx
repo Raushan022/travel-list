e9203b2b60f805c6e8ba02878d746e68391c9d87
raushan <kraushan022@gmail.com>
2026-01-16 15:07:44 +0530

    UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackagingList />
+      <Stats />
+    </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴Far Away💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackagingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        💼You have X items on your list, and you have already packed X (X%)
+      </em>
+    </footer>
+  );
+}
