e11b447c3521ea122c0408f1eda6356a3531c163
raushan <kraushan022@gmail.com>
2026-01-16 15:45:39 +0530

    rendering item list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "charger", quantity: 2, packed: true },
+  { id: 4, description: "mobile", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -24,7 +31,26 @@ function Form() {
 }
 
 function PackagingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
